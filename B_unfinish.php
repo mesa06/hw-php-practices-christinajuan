c89d0bb735e0bfdd6ab7e7985386516907768389
christina小龍 <christjuan1104@gmail.com>
2016-10-17 15:05:34 +0000

    說明事項

---
+++ B_unfinish.php
@@ -0,0 +1,30 @@
+<!--B. 寫程式算一下: 利用1,2,3,4這四個數字，可組出多少個數字互不相同三位數?-->
+<?php
+function take(){
+	$result="1234";
+	
+}
+// $sData = "";
+// $f = fopen("pick3.txt", "r");
+// while (!feof($f))
+// {
+// 	$line = fgets($f);
+// 	$sData .= Trim($line);
+// }
+// fclose($f);
+// echo HotCodeList($sData);
+
+
+// function HotCodeList($sData)
+// {
+// 	$result = "01234567890";
+// 	$iLen = strlen($sData);
+// 	for ($iPos = 0; $iPos < $iLen; $iPos++)
+// 	{
+// 		$ch = substr($sData, $iPos, 1);
+// 		$result = $ch . str_replace($ch, "", $result);
+	
+// 	}
+// 	return substr($result, 0, 5) . "-" . substr($result, 5, 5);
+// }
+?>
